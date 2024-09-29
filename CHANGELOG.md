63465d41eb1a94dce74d02575e9823c61094eca5
Ravi Kant <46199672+Rvkt@users.noreply.github.com>
2024-09-29 21:10:52 +0530

    :sparkles: implement PopScope

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,8 @@ All notable changes to this project will be documented in this file.
 ## [1.0.0+1] - 2024-09-29
 ### Added
 - Initial release with basic project code.
+
+
+## [1.0.0+2] - 2024-09-29
+### Added
+- Implemented `PopScope` to handle back navigation and confirm user action before exiting the app.
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -36,36 +37,83 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('apryt'),
-        centerTitle: true,
-        elevation: 4,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome to apryt!',
-                style: Theme.of(context).textTheme.displayLarge,
-                textAlign: TextAlign.center,
+    Future<bool?> _showDialog() {
+      return showDialog<bool>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Exit the App?'),
+            content: const Text('Are you sure you want to leave the app?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('Yes, exit the app'),
+                onPressed: () {
+                  // Return true to indicate the user confirmed exit
+                  Navigator.pop(context, true);
+                },
               ),
-              const SizedBox(height: 16),
-              Text(
-                'Your playground for Flutter concepts and experimentation.',
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.center,
+              TextButton(
+                child: const Text('No, stay in the app'),
+                onPressed: () {
+                  // Return false to indicate the user canceled the exit
+                  Navigator.pop(context, false);
+                },
               ),
             ],
+          );
+        },
+      );
+    }
+
+
+    return PopScope(
+      canPop: false,
+      onPopInvokedWithResult: (bool didPop, Object? result) async {
+        if (didPop) {
+          return;
+        }
+        final bool shouldPop = await _showDialog() ?? false;
+        if (shouldPop) {
+          // Since this is the root route, quit the app where possible by
+          // invoking the SystemNavigator. If this wasn't the root route,
+          // then Navigator.maybePop could be used instead.
+          // See https://github.com/flutter/flutter/issues/11490
+          SystemNavigator.pop();
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('apryt'),
+          centerTitle: true,
+          elevation: 4,
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome to apryt!',
+                  style: Theme.of(context).textTheme.displayLarge,
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  'Your playground for Flutter concepts and experimentation.',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ),
